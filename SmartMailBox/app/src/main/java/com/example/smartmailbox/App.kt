3ada40517bac68d6e71362a8a2d22122cc1fc71b
Maercel <marcel.majhenic@gmail.com>
2026-05-25 18:05:56 +0200

    feat(login): add login ui and api logic

--- SmartMailBox/app/src/main/java/com/example/smartmailbox/App.kt
+++ SmartMailBox/app/src/main/java/com/example/smartmailbox/App.kt
@@ -5,17 +5,21 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.smartmailbox.navigation.NavigationScreen
 import com.example.smartmailbox.ui.theme.SmartMailBoxTheme
 import com.example.smartmailbox.view.HomeView
 import com.example.smartmailbox.view.LogView
+import com.example.smartmailbox.view.LoginView
 import com.example.smartmailbox.view.MailBoxView
 import com.example.smartmailbox.viewmodel.HomeViewModel
 import com.example.smartmailbox.viewmodel.LogViewModel
+import com.example.smartmailbox.viewmodel.LoginViewModel
 import com.example.smartmailbox.viewmodel.MailBoxViewModel
 
 
@@ -32,21 +36,55 @@ fun App() {
     val homeViewModel: HomeViewModel = viewModel()
     val mailBoxViewModel: MailBoxViewModel = viewModel()
     val logModel: LogViewModel = viewModel()
+    val loginModel: LoginViewModel = viewModel()
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    val showMainBars = currentRoute != NavigationScreen.Login.route
 
     SmartMailBoxTheme {
         Scaffold(
-            topBar = { TopAppBar() },
-            bottomBar = { AppFooter(navController = navController) }
+            topBar = {
+                if (showMainBars) {
+                    TopAppBar()
+                }
+            },
+            bottomBar = {
+                if (showMainBars) {
+                    AppFooter(navController = navController)
+                }
+            }
         ) { paddingValues ->
             NavHost(
                 navController = navController,
-                startDestination = NavigationScreen.Home.route,
+                startDestination = NavigationScreen.Login.route,
                 enterTransition = { fadeIn(tween(200)) },
                 exitTransition = { fadeOut(tween(200)) },
                 popEnterTransition = { fadeIn(tween(200)) },
                 popExitTransition = { fadeOut(tween(200)) }
             ) {
                 // navigateBack exists
+                composable(NavigationScreen.Login.route) {
+                    LoginView(
+                        loginViewModel = loginModel,
+                        onLoginSuccess = {
+                            navController.navigate(NavigationScreen.Home.route) {
+                                popUpTo(NavigationScreen.Login.route) {
+                                    inclusive = true // So backstack becomes Home (it removes Login)
+                                }
+                            }
+                        },
+                        onTwoFactorRequired = {
+                            // temporary until we add face-recognition
+                            navController.navigate(NavigationScreen.Home.route) {
+                                popUpTo(NavigationScreen.Login.route) {
+                                    inclusive = true
+                                }
+                            }
+                        }
+                    )
+                }
                 composable(NavigationScreen.Home.route) { HomeView(homeViewModel, paddingValues) }
                 composable(NavigationScreen.Scan.route) { MailBoxView(mailBoxViewModel, paddingValues) }
                 composable(NavigationScreen.Log.route) { LogView(logModel, paddingValues) }
--- SmartMailBox/app/src/main/java/com/example/smartmailbox/navigation/NavigationScreen.kt
+++ SmartMailBox/app/src/main/java/com/example/smartmailbox/navigation/NavigationScreen.kt
@@ -1,7 +1,10 @@
 package com.example.smartmailbox.navigation
 
+import androidx.navigation.Navigation
+
 sealed class NavigationScreen(val route: String) {
     object Home : NavigationScreen("home")
     object Scan : NavigationScreen("scan")
     object Log : NavigationScreen("log")
+    object Login : NavigationScreen("login")
 }
